1605bd7b55f4f6f99139606edc679b626679fb79
kirill sychev <kirill>
2022-11-12 13:39:02 +0500

    удаление накладных

---
+++ Abstractions/IInvoiceService.cs
@@ -0,0 +1,18 @@
+﻿namespace logistics_system_back.Abstractions
+{
+    using DataTransferObjects;
+    using Models;
+
+    /// <summary>
+    /// Интерфейс сервиса работы с позициями накладных
+    /// </summary>
+    public interface IInvoiceService
+    {
+
+        /// <summary>
+        /// Удаление накладной
+        /// </summary>
+        void RemoveInvoice(Guid invoiceId);
+
+    }
+}
---
+++ Services/InvoiceService.cs
@@ -0,0 +1,21 @@
+﻿namespace logistics_system_back.Services
+{
+    using Models;
+
+    public class InvoiceService
+    {
+        private readonly ApplicationContext _db;
+
+        public InvoiceService(ApplicationContext context)
+        {
+            _db = context;
+        }
+
+        public void RemoveInvoice(Guid invoiceId)
+        {
+            Invoice invoice = _db.Invoices.Where(x => x.Id == invoiceId).First();
+            _db.Invoices.Remove(invoice);
+            _db.SaveChanges();
+        }
+    }
+}
